10d09162b71218de4999954472980891d385b91a
Gauranga Das <dasgauranga2@gmail.com>
2021-01-25 19:27:46 +0530

    Basic Layout Design

--- app/src/main/java/com/example/glowchat/MainActivity.java
+++ app/src/main/java/com/example/glowchat/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.glowchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login,signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.loginButton);
+        signup = findViewById(R.id.signupButton);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
