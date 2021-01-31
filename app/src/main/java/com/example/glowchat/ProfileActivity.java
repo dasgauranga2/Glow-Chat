0eccc9f86967fce66e6a174026705457f3bfb8e2
Gauranga Das <dasgauranga2@gmail.com>
2021-01-31 11:35:35 +0530

    Profile Activity styling

--- app/src/main/java/com/example/glowchat/ProfileActivity.java
+++ app/src/main/java/com/example/glowchat/ProfileActivity.java
@@ -12,16 +12,25 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    EditText username;
+    EditText username,status;
     FirebaseDatabase database;
-    DatabaseReference ref;
+    DatabaseReference username_ref,status_ref;
     String current_user;
 
     public void save_profile(View view) {
 
         String un = username.getText().toString();
+        String st = status.getText().toString();
 
-        ref.child(current_user).setValue(un);
+        if (un.length() > 0) {
+            username_ref.child(current_user).setValue(un);
+        }
+        if (st.length() > 0) {
+            status_ref.child(current_user).setValue(st);
+        }
+
+        Intent intent = new Intent(ProfileActivity.this, ChatListActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -29,9 +38,11 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        username = findViewById(R.id.usernameInput);
+        username = findViewById(R.id.usernameTextInput);
+        status =  findViewById(R.id.statusTextInput);
         database = FirebaseDatabase.getInstance();
-        ref = database.getReference("USERNAMES");
+        username_ref = database.getReference("USERNAMES");
+        status_ref = database.getReference("STATUS");
 
         Intent intent = getIntent();
         current_user = intent.getStringExtra("current_user").split("@")[0];
